d2773880658ae0b25a6b86524112fe189a470b5e
tylerHerron <54856382+tylerHerron@users.noreply.github.com>
2021-08-09 13:46:17 -0500

    added comments

--- classes.js
+++ classes.js
@@ -1,7 +1,8 @@
-class ServerEvent{
-    constructor(matchDate, eventTime, player, trigger, target, gun, blindedTime, headshot, penetrated, flashIndex, round){
+class ServerEvent {
+    constructor(matchDate, eventTime, player, trigger, target, gun, blindedTime, headshot, penetrated, flashIndex, round) {
         //this.matchDate = matchDate;
         //this.eventTime = eventTime;
+        //new comment
         this.player = player.split("<")[0];
         this.trigger = trigger;
         this.target = target.split("<")[0];
